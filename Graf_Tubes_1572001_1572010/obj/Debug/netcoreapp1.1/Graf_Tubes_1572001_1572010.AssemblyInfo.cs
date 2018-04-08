b7f6ee94991574a88b4c4467ed3a1e466aa40b4a
DESKTOP-NIAGVVD\lisansulistiani <lisans1601@gmail.com>
2018-04-08 20:44:10 +0700

    delete project gak kepake

--- Graf_Tubes_1572001_1572010/obj/Debug/netcoreapp1.1/Graf_Tubes_1572001_1572010.AssemblyInfo.cs
+++ Graf_Tubes_1572001_1572010/obj/Debug/netcoreapp1.1/Graf_Tubes_1572001_1572010.AssemblyInfo.cs
@@ -13,7 +13,6 @@ using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Graf_Tubes_1572001_1572010")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyDescriptionAttribute("Package Description")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Graf_Tubes_1572001_1572010")]
